df8973eea39045b08c2e093f60b883255f83910b
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2024-12-01 20:18:26 -0300

    exception-and-log-standart

--- data_pipeline_project/etl/extract/appsheet.py
+++ data_pipeline_project/etl/extract/appsheet.py
@@ -2,10 +2,11 @@ import requests
 
 
 class AppSheetExtractor:
-    def __init__(self, api_url, api_key, app_id):
+    def __init__(self, api_url, api_key, app_id, logger):
         self.api_url = api_url
         self.api_key = api_key
         self.app_id = app_id
+        self.logger = logger
 
     def extract(self, table_name):
         endpoint = f'{self.api_url}/{self.app_id}/tables/{table_name}/Action'
@@ -24,8 +25,12 @@ class AppSheetExtractor:
             response = requests.post(endpoint, headers=headers, json=payload)
             response.raise_for_status()
             data = response.json()
-            return None, data
-        except requests.exceptions.HTTPError as http_err:
-            return http_err, None
+            self.logger.info(
+                f'Data extracted successfully from {table_name}'
+            )
+            return data
         except Exception as err:
-            return err, None
+            self.logger.error(
+                f'Failed to extract data from {table_name}: {err}'
+            )
+            raise ValueError(err)
--- data_pipeline_project/etl/loading/upload_manager.py
+++ data_pipeline_project/etl/loading/upload_manager.py
@@ -2,17 +2,22 @@ import boto3
 
 
 class UploadManager:
-    def __init__(self):
-        self.s3_client = boto3.client('s3')
+    def __init__(self, logger):
+        self.logger = logger
+        try:
+            self.s3_client = boto3.client('s3')
+        except Exception as e:
+            self.logger.error(f'Failed to initialize UploadManager: {e}')
+            raise ValueError(e)
 
-    def upload_to_s3(self, file_path, bucket_name, s3_key, logger):
-        try: 
+    def upload_to_s3(self, file_path, bucket_name, s3_key):
+        try:
             self.s3_client.upload_file(file_path, bucket_name, s3_key)
-            logger.info(
+            self.logger.info(
                 f'Successfully uploaded {file_path} to s3://{bucket_name}/{s3_key}'
             )
         except Exception as e:
-            logger.error(
+            self.logger.error(
                 f'Failed to upload {file_path} to s3://{bucket_name}/{s3_key}: {e}'
             )
-            raise
+            raise ValueError(e)
